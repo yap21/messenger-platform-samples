0d8cb065a5a1929d2d3caffc2716ef9e7eb21bae
김주호/기타 <jhkim@humanworks.com>
2017-06-15 12:57:38 +0900

    'test'

--- app.js
+++ app.js
@@ -121,14 +121,14 @@ app.post('/webhook', function (req, res) {
       });
     });
 
-    console.log(data.object);
 
     // Assume all went well.
     //
-    // You must send back a 200, within 20 seconds, to let us know you've 
+    // You must send back a 200, within 20 seconds, to let us know you've
     // successfully received the callback. Otherwise, the request will time out.
     res.sendStatus(200);
   }
+  console.log(data.object);
 });
 
 /*
